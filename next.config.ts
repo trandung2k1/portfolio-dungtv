a57e2973fd105c120464fe415f12be1f9e13f551
trandung2k1 <trandungitksnb2001@gmail.com>
2025-05-02 21:32:08 +0700

    update code

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  allowedDevOrigins: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://54.161.63.190:4000'],
 };
 
 export default nextConfig;
